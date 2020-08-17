356e33a1b9b194788544f21ba5047f4eec72eb60
julioju <41541427+Julioju1015@users.noreply.github.com>
2020-08-17 16:20:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,35 +24,6 @@ fs.readdir('./Events/', (error, f) => {
     });
 });
 
-/////// PARTIE ERREURS //////
-
-client.utils = new Discord.Collection();
-
-fs.readdir('./utils/', (err, files) =>{
-    if(err) throw err;
-    for(let file of files) {
-        if(!file.endsWith(".js")) return;
-
-        let props = require(`./utils/${file}`);
-        let commandName = props.help.name;
-
-        client.utils.set(commandName, props);
-
-    };
-})
-
-process.on('unhandledRejection', error => {
-    console.error('✅ Erreur d\'API détéctée : ' + error);
-
-    const get_channel_guild = client.channels.cache.get(error.path.slice(10, 28)).guild
-
-    const api_error_embed = new Discord.MessageEmbed()
-        .setColor("#2f3136")
-        .setDescription("🔺 **Il y a eu une erreur d'API sur le bot :**\n```js\n" + error + "```")
-        .addField("🔺 Précisions :", "```\nNom : " + error.name + "\nMessage : " + error.message + "\nPath : " + error.path + "\nCode d'erreur : " + error.code + "\nMéthode : " + error.method + "```")
-        .addField("🔺 Localisation :", "`Channel :` <#" + error.path.slice(10, 28) + ">, `ID :` " + error.path.slice(10, 28) + "\n`Serveur :` " + get_channel_guild.name + ", `ID :` " + get_channel_guild.id)
-    client.channels.cache.get("734763152104751144").send("@here", api_error_embed)
-});
 
 ////TOKEN DU BOT//////////
-client.login('NzIzNTgyOTU0MjQ4NDcwNjI5.XuzvIw.CgU2Dxv5A3VRT05W8dw3J6tAdbQ')
+client.login('Votre token')
